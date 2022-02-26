37462f44e56f51b5ed72e5f074a4d1e560580c81
rajbeladiya4 <20ce003@charusat.edu.in>
2022-02-26 13:49:17 +0530

    new folder

---
+++ assets/js/menu_item.js
@@ -0,0 +1,23 @@
+var noti = document.querySelector('h5');
+	var select = document.querySelector('.select');
+	var button = document.getElementsByClassName('add-to-cart');
+	for(var but of button){
+		but.addEventListener('click', (e)=>{
+			var add = Number(noti.getAttribute('data-count') || 0);
+			noti.setAttribute('data-count', add +1);
+			noti.classList.add('zero')
+
+
+			// copy and paste //
+			var parent = e.target.parentNode;
+			var clone = parent.cloneNode(true);
+			select.appendChild(clone);
+			clone.lastElementChild.innerText = "Buy-now";
+			
+			if (clone) {
+				noti.onclick = ()=>{
+					select.classList.toggle('display');
+				}	
+			}
+		})
+	}
